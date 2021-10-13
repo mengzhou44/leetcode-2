978b2e7fac8ef0f7273d6f0519ac514b63852bdc
Meng Zhou <v-menzhou@expediagroup.com>
2021-10-12 23:03:29 -0500

    commit

---
+++ 121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+var maxProfit = function (prices) {
+  let profit = 0;
+
+  let minPrice = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(prices[i], minPrice);
+    if (prices[i] > minPrice) {
+      profit = Math.max(profit, prices[i] - minPrice);
+    }
+  }
+
+  return profit;
+};
